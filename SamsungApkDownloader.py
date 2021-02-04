31440425417ebb792b56e846ee93866166eaa531
David Lev <davidl@hadran.net>
2021-02-04 17:01:03 +0200

    Strings changes

--- SamsungApkDownloader.py
+++ SamsungApkDownloader.py
@@ -1,16 +1,19 @@
+import os
 import re
 from urllib import request
 import requests
 
+# set print colors
+yellow, red, blue, green, end = '\033[93m', '\033[91m', '\033[94m', '\033[92m', '\033[0m'
 # set url format
 url_format = "https://vas.samsungapps.com/stub/stubDownload.as?appId={}&deviceId={}" \
              "&mcc=425&mnc=01&csc=ILO&sdkVer={}&pd=0&systemId=1608665720954&callerId=com.sec.android.app.samsungapps" \
              "&abiType=64&extuk=0191d6627f38685f"
 
 # get input from user
-package_name = str(input("Enter the package name (samsung app only): "))
-model = str(input("Enter the device model (SM-XXXXX format): "))
-sdk_ver = str(input("Enter the android version (SDK format - 26, 28, 29 etc..): "))
+package_name = str(input(f"{green}Enter the package name (samsung app only): "))
+model = str(input(f"{green}Enter the device model (SM-XXXXX format): "))
+sdk_ver = str(input(f"{green}Enter the android version (SDK format - 26, 28, 29 etc..): "))
 
 url = url_format.format(package_name, model, sdk_ver)
 # get the text from xml respond
@@ -30,27 +33,26 @@ def main():
         # print error message from samsung
         error_msg = re.compile(r"resultMsg>(.*)</resultMsg>").findall(url)
         if error_msg:
-            print(error_msg[0])
+            print(f'{blue}Samsung Servers: {red}"{error_msg[0]}"{end}')
             return
         #
-        print("No results!")
+        print(f"{red}No results!{end}")
         return
 
     match = match[0]
 
     # print the available apk file
-    print(f"\nThe version code is: {match['vs_code']}\nThe version name is: {match['vs_name']}\n")
-    continue_msg = input("Enter [Y/n] to download: ")
+    print(f"{blue}\nThe available version Code is: {yellow}{match['vs_code']}\n{blue}The available Version Name is: {yellow}{match['vs_name']}{end}\n")
+    continue_msg = input(f"{blue}Do you want to download? {yellow}[Y/n]: ")
     # download the apk file
     if continue_msg in ("Y", "y"):
-        print("\nDownload started!")
+        print(f"\n{blue}Download started!...{end}")
         file = request.urlretrieve(match["uri"], f'{package_name}-{match["vs_code"]}.apk')
-        yellow, end = '\033[93m', '\033[0m'
-        print(f"APK saved: {yellow}{file[0]}{end}")
+        print(f"{blue}APK saved: {yellow}{os.getcwd()}/{file[0]}{end}")
     elif continue_msg in ("N", "n"):
-        print("OK!")
+        print(f"{blue}Okay, You can try me again any time :){end}")
     else:
-        print("error input")
+        print(f"{red}Error input | enter 'Y' or 'N'{end}")
 
 
 if __name__ == '__main__':
