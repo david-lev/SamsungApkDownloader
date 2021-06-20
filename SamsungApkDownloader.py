6d9580cd52d5a7967a7c91412771d91c6be3d5ba
David Lev <davidlev050414@gmail.com>
2021-06-20 13:03:50 +0300

    model name to uppercase, strings changes

--- SamsungApkDownloader.py
+++ SamsungApkDownloader.py
@@ -10,15 +10,16 @@ url_format = "https://vas.samsungapps.com/stub/stubDownload.as?appId={}&deviceId
              "&mcc=425&mnc=01&csc=ILO&sdkVer={}&pd=0&systemId=1608665720954&callerId=com.sec.android.app.samsungapps" \
              "&abiType=64&extuk=0191d6627f38685f"
 
-# get input from user
-package_name = str(input(f"{green}Enter the package name (Samsung apps only): {end}"))
-model = str(input(f"{green}Enter the device model ({yellow}SM-XXXXX{green} format): {end}"))
-sdk_ver = str(input(f"{green}Enter the android version (SDK format - {yellow}26 28 29{green} etc..): {end}"))
+# Get some input from the user
+package_name = str(input(f"{green}Enter package name (Samsung apps only): {end}"))
+model = str(input(f"{green}Enter your device model ({yellow}SM-XXXXX{green} format): {end}"))
+model = model.upper()
+sdk_ver = str(input(f"{green}Enter your android version (SDK format - {yellow}26 28 29{green} etc..): {end}"))
 
 url = url_format.format(package_name, model, sdk_ver)
 # get the text from xml respond
 url = requests.get(url).text
-# input the info from the xml into dict
+# input the info from the xml into a dict
 regex_info = re.compile(r"resultCode>(?P<status>\d+)</resultCode>"
                         r".*<resultMsg>(?P<msg>[^']*)</resultMsg>"
                         r".*<downloadURI><!\[CDATA\[(?P<uri>[^']*)\]\]></downloadURI>"
@@ -30,7 +31,7 @@ regex_info = re.compile(r"resultCode>(?P<status>\d+)</resultCode>"
 def main():
     match = [m.groupdict() for m in regex_info.finditer(url)]
     if not match:
-        # print error message from samsung
+        # Showing error message from samsung servers
         error_msg = re.compile(r"resultMsg>(.*)</resultMsg>").findall(url)
         if error_msg:
             print(f'{blue}Samsung Servers: {red}"{error_msg[0]}"{end}')
@@ -41,12 +42,12 @@ def main():
 
     match = match[0]
 
-    # print the available apk file
-    print(f"{blue}\nThe available version Code is: {yellow}{match['vs_code']}"
-          f"\n{blue}The available Version Name is: {yellow}{match['vs_name']}{end}\n")
+    # Print the available apk file info
+    print(f"{blue}\nThe available VersionCode is: {yellow}{match['vs_code']}"
+          f"\n{blue}The available VersionName is: {yellow}{match['vs_name']}{end}\n")
     continue_msg = input(f"{blue}Do you want to download? {yellow}[Y/n]: ")
-    # download the apk file
-    if continue_msg in ("Y", "y"):
+    # Download the apk file
+    if continue_msg in ("Y", "y", ""):
         print(f"\n{blue}Download started!...{end}")
         file = request.urlretrieve(match["uri"], f'{package_name}-{match["vs_code"]}.apk')
         print(f"{blue}APK saved: {yellow}{os.getcwd()}/{file[0]}{end}")
