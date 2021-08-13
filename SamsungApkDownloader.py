62d560195e4c3ab771e102cd1bcf9d96e39e7c2d
David Lev <davidlev050414@gmail.com>
2021-08-13 17:33:02 +0300

    Adding argument method & fix bugs

--- SamsungApkDownloader.py
+++ SamsungApkDownloader.py
@@ -2,6 +2,8 @@ import os
 import re
 from urllib import request
 import requests
+import argparse
+from sys import argv
 
 # set print colors
 yellow, red, blue, green, end = '\033[93m', '\033[91m', '\033[94m', '\033[92m', '\033[0m'
@@ -11,12 +13,27 @@ url_format = "https://vas.samsungapps.com/stub/stubDownload.as?appId={}&deviceId
              "&abiType=64&extuk=0191d6627f38685f"
 
 # Get some input from the user
-package_name = str(input(f"{green}Enter package name (Samsung apps only): {end}"))
-model = str(input(f"{green}Enter your device model ({yellow}SM-XXXXX{green} format): {end}"))
-model = model.upper()
-sdk_ver = str(input(f"{green}Enter your android version (SDK format - {yellow}26 28 29{green} etc..): {end}"))
+if len(argv) > 1:
+    my_parser = argparse.ArgumentParser(prog='SamsungApkDownloader.py',
+                                        description='List of arguments for SamsungAPkDownloader')
 
-url = url_format.format(package_name, model, sdk_ver)
+    my_parser.add_argument("-m", type=str, metavar="'device-model'", help="Samsung model in  'SM-XXXXX' format",
+                           required=True)
+    my_parser.add_argument("-v", type=int, metavar="'sdk-version'", help="Android sdk version (19-30)",
+                           required=True)
+    my_parser.add_argument("-p", type=str, metavar="'package-name'", help="Package Name 'com.package.name'",
+                           required=True)
+
+    args = my_parser.parse_args()
+    package_name = args.p
+    model = args.m
+    sdk_ver = args.v
+else:
+    package_name = str(input(f"{green}Enter package name (Samsung apps only): {end}"))
+    model = str(input(f"{green}Enter your device model ({yellow}SM-XXXXX{green} format): {end}"))
+    sdk_ver = str(input(f"{green}Enter your android version (SDK format - {yellow}26 28 29{green} etc..): {end}"))
+
+url = url_format.format(package_name, model.upper(), sdk_ver)
 # get the text from xml respond
 url = requests.get(url).text
 # input the info from the xml into a dict
@@ -27,7 +44,7 @@ regex_info = re.compile(r"resultCode>(?P<status>\d+)</resultCode>"
                         r".*<versionName>(?P<vs_name>[^']*)</versionName>", re.MULTILINE | re.DOTALL)
 
 
-# dill with error cases
+# deal with error cases
 def main():
     match = [m.groupdict() for m in regex_info.finditer(url)]
     if not match:
@@ -43,18 +60,19 @@ def main():
     match = match[0]
 
     # Print the available apk file info
-    print(f"{blue}\nThe available VersionCode is: {yellow}{match['vs_code']}"
-          f"\n{blue}The available VersionName is: {yellow}{match['vs_name']}{end}\n")
+    print(f"{blue}\nThe available versionCode is: {yellow}{match['vs_code']}"
+          f"\n{blue}The available versionName is: {yellow}{match['vs_name']}{end}\n")
     continue_msg = input(f"{blue}Do you want to download? {yellow}[Y/n]: ")
     # Download the apk file
-    if continue_msg in ("Y", "y", ""):
-        print(f"\n{blue}Download started!...{end}")
-        file = request.urlretrieve(match["uri"], f'{package_name}-{match["vs_code"]}.apk')
-        print(f"{blue}APK saved: {yellow}{os.getcwd()}/{file[0]}{end}")
-    elif continue_msg in ("N", "n"):
-        print(f"{blue}Okay, You can try me again any time :){end}")
+    while continue_msg not in ["Y", "y", "", "N", "n"]:
+        continue_msg = input(f"{blue}Error input. choose {yellow}[Y/n]: ")
     else:
-        print(f"{red}Error input | enter 'Y' or 'N'{end}")
+        if continue_msg in ("Y", "y", ""):
+            print(f"\n{blue}Download started!...{end}")
+            file = request.urlretrieve(match["uri"], f'{package_name}-{match["vs_code"]}.apk')
+            print(f"{blue}APK saved: {yellow}{os.getcwd()}/{file[0]}{end}")
+        elif continue_msg in ("N", "n"):
+            print(f"{blue}Okay, You can try me again any time :){end}")
 
 
 if __name__ == '__main__':
